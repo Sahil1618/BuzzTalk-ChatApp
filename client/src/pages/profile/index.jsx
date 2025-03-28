02ccc1d292e87f3e514ac9cebbaefbe1f17df82b
Sahil1618 <ss8876584@gmail.com>
2025-03-28 11:57:02 +0530

    BuzzTalk first commit

---
+++ client/src/pages/profile/index.jsx
@@ -0,0 +1,14 @@
+import { useAppStore } from "@/store"
+
+const Profile = () => {
+  const {userInfo}= useAppStore()
+  return (
+    <div>Profile
+    <div>
+      Email:{userInfo.id}
+    </div></div>
+    
+  )
+}
+
+export default Profile
