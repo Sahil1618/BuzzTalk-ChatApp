7db09b3caeabd9c75b2ea49ea7b30f967720ed1c
Sahil1618 <ss8876584@gmail.com>
2025-04-04 17:19:30 +0530

    Implemented socket setup with send/receive message functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Toaster } from "sonner";
+import "./index.css";
+import { Toaster } from "./components/ui/sonner.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <>
+  <SocketProvider>
     <App />
     <Toaster closeButton />
-  </>
+  </SocketProvider>
 
   // </StrictMode>,
 );
