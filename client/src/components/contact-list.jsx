a6e569f0722d757eecb703dea610a85fd9ba531f
Sahil1618 <ss8876584@gmail.com>
2025-04-07 17:07:57 +0530

    Added Get users and message fetching functionality

---
+++ client/src/components/contact-list.jsx
@@ -0,0 +1,30 @@
+import { useAppStore } from "@/store";
+
+const ContactList = ({ contacts, isChannel = false }) => {
+  const {
+    selectedChatData,
+    setSelectedChatData,
+    setSelectedChatType,
+    selectedChatType,
+    setSelectedChatMessages,
+  } = useAppStore();
+
+  const handleClick = (contact) => {
+    if (isChannel) setSelectedChatType("channel");
+    else selectedChatType("contact");
+    setSelectedChatData(contact);
+    if (selectedChatData && selectedChatData._id !== contact._id) {
+      setSelectedChatMessages([]);
+    }
+  };
+
+  return (
+    <div className="mt-5">
+      {contacts.map((contact) => (
+        <div key={contact._id}>{contact._id}</div>
+      ))}
+    </div>
+  );
+};
+
+export default ContactList;
--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,16 +1,38 @@
+import { useEffect } from "react";
 import NewDM from "./components/new-dm";
 import ProfileInfo from "./components/profile-info";
+import { apiClient } from "@/lib/api-client";
+import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
+import { useAppStore } from "@/store";
+import ContactList from "@/components/contact-list";
 
 const ContactsContainer = () => {
+  const { setDirectMessagesContacts, directMessagesContacts } = useAppStore();
+  useEffect(() => {
+    const getContacts = async () => {
+      const response = await apiClient.get(GET_DM_CONTACTS_ROUTES, {
+        withCredentials: true,
+      });
+      if (response.data.contacts) {
+        setDirectMessagesContacts(response.data.contacts);
+      }
+    };
+
+    getContacts();
+  }, []);
+
   return (
     <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
       <div className="pt-3">
-        <Logo/>
+        <Logo />
       </div>
       <div className="my-5">
         <div className="flex items-center justify-between pr-10">
           <Title text="Direct Messages" />
-          <NewDM/>
+          <NewDM />
+        </div>
+        <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
+          <ContactList contacts={directMessagesContacts}/>
         </div>
       </div>
       <div className="my-5">
@@ -18,7 +40,7 @@ const ContactsContainer = () => {
           <Title text="Channels" />
         </div>
       </div>
-      <ProfileInfo/>
+      <ProfileInfo />
     </div>
   );
 };
@@ -58,9 +80,10 @@ const Logo = () => {
   );
 };
 
-const Title = ({text}) => {
+const Title = ({ text }) => {
   return (
-    <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">{text}</h6>
-  )
-}
-
+    <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-sm">
+      {text}
+    </h6>
+  );
+};
